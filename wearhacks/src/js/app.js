d1a44f2a5170d021e9ed1576f3dd9d0a7c8b080e
Tony <tzhang995@gmail.com>
2016-03-19 05:31:17 -0400

    website:

--- wearhacks/src/js/app.js
+++ wearhacks/src/js/app.js
@@ -2,16 +2,17 @@ console.log('Sending data to Pebble...');
 require('firebase');
 
 Firebase.INTERNAL.forceWebSockets();
-var ref = new Firebase("https://sizzling-heat-5359.firebaseio.com/");
+var ref = new Firebase("https://blistering-heat-4723.firebaseio.com/");
 ref.set({ name: "PebbleB" });
 // Listen for realtime changes
-ref.on("value", function(data) {
-  var name = data.val().name;
-  console.log("My name is " + name);
+ref.on('value', function(dataSnapshot) {
+  var name = dataSnapshot->key();
+  var text = dataSnapshot->val();
+  console.log("My name is " + name + " And this is the text" + text);
 });
 
 var method = 'GET';
-var url = 'http://example.com';
+var url = 'http://discover-380bg7t8.cloudapp.net/';
 
 // Create the request
 var request = new XMLHttpRequest();
